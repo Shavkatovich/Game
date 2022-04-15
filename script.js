28b418677d3044a6a519365e666e7255f9ecebf2
Shavkatovich <khusniddin2019@mail.ru>
2022-04-16 00:52:59 +0500

    script

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const choices = document.querySelectorAll('.choice'),
+window.addEventListener('DOMContentLoaded', () => {
+    const choices = document.querySelectorAll('.choice'),
     score = document.querySelector('#score'),
     modal =  document.querySelector('.modal'),
     result = document.querySelector('#result'),
@@ -9,110 +10,111 @@ const choices = document.querySelectorAll('.choice'),
         draw: 0
     };
 
-// Play game
-function play(event) {
-    restart.style.display = 'inline-block'
-    const playerChoice = event.target.id
-    const computerChoice = getComputerChoice()
-    const winner = getWinner(playerChoice, computerChoice)
-    showWinner(winner, computerChoice)
-}
-
-
-// GetComputerChoice
-function getComputerChoice() {
-    const rand = Math.random()
-    if (rand < 0.34) {
-        return 'rock'
-    } else if (rand <=0.67) {
-        return 'paper'
-    }else {
-        return 'scissors'
+    // Play game
+    function play(event) {
+        restart.style.display = 'inline-block'
+        const playerChoice = event.target.id
+        const computerChoice = getComputerChoice()
+        const winner = getWinner(playerChoice, computerChoice)
+        showWinner(winner, computerChoice)
     }
-}
 
 
-// GetWinner
-function getWinner(p, c) {
-    if (p === c) {
-        return 'draw'
-    }else if (p === 'rock'){
-        if (c === 'paper'){
-            return 'computer'
+    // GetComputerChoice
+    function getComputerChoice() {
+        const rand = Math.random()
+        if (rand < 0.34) {
+            return 'rock'
+        } else if (rand <=0.67) {
+            return 'paper'
         }else {
-            return 'player'
-        }
-    }else if (p === 'paper'){
-        if (c === 'scissors'){
-            return 'computer'
-        }else{
-            return 'player'
-        }
-    }else if (p === 'scissors'){
-        if (c === 'rock'){
-            return 'computer'
-        }else{
-            return 'player'
+            return 'scissors'
         }
     }
-}
 
 
-// ShowWinner
-function showWinner(winner, computerChoice) {
-    if (winner === 'player') {
-        scoreBoard.player++
-        result.innerHTML = `
-            <h1 class="text-win">You win</h1>
-            <i class="fas fa-hand-${computerChoice} fa-10x"></i>
-            <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong></p>
+    // GetWinner
+    function getWinner(p, c) {
+        if (p === c) {
+            return 'draw'
+        }else if (p === 'rock'){
+            if (c === 'paper'){
+                return 'computer'
+            }else {
+                return 'player'
+            }
+        }else if (p === 'paper'){
+            if (c === 'scissors'){
+                return 'computer'
+            }else{
+                return 'player'
+            }
+        }else if (p === 'scissors'){
+            if (c === 'rock'){
+                return 'computer'
+            }else{
+                return 'player'
+            }
+        }
+    }
+
+
+    // ShowWinner
+    function showWinner(winner, computerChoice) {
+        if (winner === 'player') {
+            scoreBoard.player++
+            result.innerHTML = `
+                <h1 class="text-win">You win</h1>
+                <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+                <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong></p>
+            `
+        }else if (winner === 'computer') {
+            scoreBoard.computer++
+            result.innerHTML = `
+                <h1 class="text-win">You lose</h1>
+                <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+                <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong></p>
+            `
+        }else {
+            scoreBoard.draw++
+            result.innerHTML = `
+                <h1 class="text-win">It's A Draw</h1>
+                <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+                <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong></p>
+            `
+        }
+        score.innerHTML = `
+            <p> Player: ${scoreBoard.player}</p>
+            <p> Computer: ${scoreBoard.computer}</p>
+            <p> Draw: ${scoreBoard.draw}</p>
         `
-    }else if (winner === 'computer') {
-        scoreBoard.computer++
-        result.innerHTML = `
-            <h1 class="text-win">You lose</h1>
-            <i class="fas fa-hand-${computerChoice} fa-10x"></i>
-            <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong></p>
-        `
-    }else {
-        scoreBoard.draw++
-        result.innerHTML = `
-            <h1 class="text-win">It's A Draw</h1>
-            <i class="fas fa-hand-${computerChoice} fa-10x"></i>
-            <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong></p>
+        modal.style.display = 'block'
+    }
+
+
+    // RestartGame
+    function restartGame() {
+        scoreBoard.player = 0
+        scoreBoard.computer = 0
+        scoreBoard.draw = 0
+        score.innerHTML = `
+            <p> Player: ${scoreBoard.player}</p>
+            <p> Computer: ${scoreBoard.computer}</p>
+            <p> Draw: ${scoreBoard.draw}</p>
         `
     }
-    score.innerHTML = `
-        <p> Player: ${scoreBoard.player}</p>
-        <p> Computer: ${scoreBoard.computer}</p>
-        <p> Draw: ${scoreBoard.draw}</p>
-    `
-    modal.style.display = 'block'
-}
 
 
-// RestartGame
-function restartGame() {
-    scoreBoard.player = 0
-    scoreBoard.computer = 0
-    scoreBoard.draw = 0
-    score.innerHTML = `
-        <p> Player: ${scoreBoard.player}</p>
-        <p> Computer: ${scoreBoard.computer}</p>
-        <p> Draw: ${scoreBoard.draw}</p>
-    `
-}
-
-
-// ClearModal
-function clearModal(event) {
-    if (event.target == modal) {
-        modal.style.display = 'none'
+    // ClearModal
+    function clearModal(event) {
+        if (event.target == modal) {
+            modal.style.display = 'none'
+        }
     }
-}
 
 
-// Event listenears
-choices.forEach(choice => choice.addEventListener('click', play))
-window.addEventListener('click', clearModal)
-restart.addEventListener('click', restartGame)
+    // Event listenears
+    choices.forEach(choice => choice.addEventListener('click', play))
+    window.addEventListener('click', clearModal)
+    restart.addEventListener('click', restartGame)
+})
